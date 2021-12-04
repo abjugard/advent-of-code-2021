580bd19ea423c48736eaa64d6a3638129358e099
Adrian Bjugård <adrian.bjugard@omegapoint.se>
2021-12-04 09:45:33 +0100

    Day 4: Gotta go fast!

--- src/day04-giant_squid.py
+++ src/day04-giant_squid.py
@@ -3,38 +3,31 @@ from santas_little_helpers import day, get_data, timed
 today = day(2021, 4)
 
 
-def calculate_score(drawn, card):
+def calculate_score(last, drawn, card):
   unmarked = [n for row in card[:5] for n in row if n not in drawn]
-  return drawn[-1] * sum(unmarked)
+  return last * sum(unmarked)
 
 
 def has_bingo(drawn, card):
-  return any(all(n in drawn for n in row) for row in card)
+  return any(row <= drawn for row in card)
 
 
 def bingo(draws, cards):
   star1_found = False
-  for drawn in [draws[0:i] for i in range(5, len(draws))]:
+  for drawn, last in [(set(draws[0:i]), draws[i-1]) for i in range(5, len(draws))]:
     for idx, card in enumerate(cards):
       if has_bingo(drawn, card):
         if len(cards) == 1 or not star1_found:
-          yield calculate_score(drawn, card)
+          yield calculate_score(last, drawn, card)
           star1_found = True
         del cards[idx]
 
 
 def parse_cards(raw_inp):
-  card = None
-  for line in raw_inp:
-    if line == '':
-      if card is not None:
-        card.extend(map(list, zip(*card)))
-        yield card
-      card = []
-      continue
-    nums = line.strip().split()
-    card.append(list(map(int, nums)))
-  yield card
+  for card_inp in raw_inp:
+    candidates = [list(map(int, row.split())) for row in card_inp.split('\n')]
+    candidates.extend(map(list, zip(*candidates)))
+    yield [frozenset(candidate) for candidate in candidates]
 
 
 def parse(raw_inp):
@@ -44,7 +37,7 @@ def parse(raw_inp):
 
 
 def main():
-  draws, cards = parse(get_data(today))
+  draws, cards = parse(get_data(today, [], groups=True))
   stars = bingo(draws, cards)
   print(f'{today} star 1 = {next(stars)}')
   print(f'{today} star 2 = {next(stars)}')
