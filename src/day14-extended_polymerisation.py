511ae93141b05e73b4ca08ebbaba87616d529606
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2021-12-14 07:22:20 +0100

    Day 14: Optimised

--- src/day14-extended_polymerisation.py
+++ src/day14-extended_polymerisation.py
@@ -4,15 +4,33 @@ from collections import Counter
 today = day(2021, 14)
 
 
-def part1(template, rules):
-  for _ in range(10):
-    s = template
-    for i, p in reversed(list(enumerate(zip(template, template[1:])))):
-      s = s[:i+1] + rules[p] + s[i+1:]
-    template = s
+def score(template, pairs):
+  counts = Counter()
+  for (left, _), count in pairs.items():
+    counts[left] += count
+  counts[template[-1]] += 1
 
-  counts = Counter(template).most_common()
-  return counts[0][1] - counts[-1][1]
+  counts = counts.most_common()
+  return (counts[0][1] - counts[-1][1])
+
+
+def expand(template, rules, checkpoints):
+  pairs = Counter()
+  for a, b in zip(template, template[1:]):
+    pairs[(a, b)] += 1
+
+  for t in range(max(checkpoints)):
+    n_pairs = Counter()
+    for (a, b), count in pairs.items():
+      if (a, b) in rules:
+        c = rules[(a, b)]
+        n_pairs[(a, c)] += count
+        n_pairs[(c, b)] += count
+      else:
+        n_pairs[(a, b)] += count
+    pairs = n_pairs
+    if t + 1 in checkpoints:
+      yield score(template, pairs)
 
 
 def parse(line):
@@ -24,7 +42,9 @@ def parse(line):
 def main():
   template, _, *rules = list(get_data(today, [('func', parse)]))
   rules = {tuple(p): c for p, c in rules}
-  print(f'{today} star 1 = {part1(template, rules)}')
+  star_gen = expand(template, rules, [10, 40])
+  print(f'{today} star 1 = {next(star_gen)}')
+  print(f'{today} star 2 = {next(star_gen)}')
 
 
 if __name__ == '__main__':
